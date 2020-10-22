36687a43b05ca82367e882850f6e26314ad2797e
darango91 <crash.diego91@gmail.com>
2020-10-22 17:39:26 -0500

    Initial version for danggo.co

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+import Navigationbar from "./components/Navigationbar";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Alert, Breadcrumb, Button, Card, Container } from "react-bootstrap";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navigationbar></Navigationbar>
+      <Container>
+        <Card className="mb-3">
+          <Card.Img />
+          <Card.Body>
+            <Card.Title>Card Example</Card.Title>
+            <Card.Text>This is an example of react bootstrap cards</Card.Text>
+            <Button variant="primary">Read more</Button>
+          </Card.Body>
+        </Card>
+        <Breadcrumb>
+          <Breadcrumb.Item>Test 1</Breadcrumb.Item>
+          <Breadcrumb.Item>Test 2</Breadcrumb.Item>
+          <Breadcrumb.Item active>Test 3</Breadcrumb.Item>
+        </Breadcrumb>
+        <Alert variant="success">This is a button</Alert>
+        <Button>Test Button</Button>
+      </Container>
     </div>
   );
 }
